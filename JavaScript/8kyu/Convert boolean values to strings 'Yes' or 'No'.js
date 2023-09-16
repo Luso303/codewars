9fc89b9aaed8a0574eb588909b713397c41a8d3a
Russell <Luso303@gmail.com>
2023-09-15 19:04:06 -0600

    convert boolean

--- JavaScript/8kyu/Convert boolean values to strings 'Yes' or 'No'.js
+++ JavaScript/8kyu/Convert boolean values to strings 'Yes' or 'No'.js
@@ -1,6 +1,8 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/javascript
+
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
-function boolToWord( bool ){
+function boolToWord(bool){
   return bool ? 'Yes' : 'No'
 }
 
@@ -8,7 +10,7 @@ function boolToWord( bool ){
 let boolToWord = bool => bool ? 'Yes' : 'No';
 
 // or if we wanted it "spelled out"
-function boolToWord( bool ){
+function boolToWord(bool){
   if (bool) {
     return 'Yes';
   } else {
